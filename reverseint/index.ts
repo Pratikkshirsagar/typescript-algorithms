1acc0e0e618056f76cc824e974e5d9cf17b8414f
Pratikkshirsagar <meetpratiik@gmail.com>
2020-07-16 22:23:14 +0530

    reverse int added

---
+++ reverseint/index.ts
@@ -0,0 +1,21 @@
+// --- Directions
+// Given an integer, return an integer that is the reverse
+// ordering of numbers.
+// --- Examples
+//   reverseInt(15) === 51
+//   reverseInt(981) === 189
+//   reverseInt(500) === 5
+//   reverseInt(-15) === -51
+//   reverseInt(-90) === -9
+
+function reverseInt(num: number): Number {
+  let reversed = num.toString().split('').reverse().join('');
+
+  if (num < 0) {
+    return parseInt(reversed) * -1;
+  }
+
+  return parseInt(reversed);
+}
+
+reverseInt(500);
