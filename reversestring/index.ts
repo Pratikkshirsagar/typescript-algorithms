12ad8e640d3dd01f6c8901a307a1cbec19d24b70
Pratikkshirsagar <meetpratiik@gmail.com>
2020-07-16 18:35:24 +0530

    second solution of string reverse added

--- reversestring/index.ts
+++ reversestring/index.ts
@@ -6,9 +6,25 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+// solution 1
 function reverse(str: String): String {
   const reverseString = str.split('').reverse().join('');
   return reverseString;
 }
 
 reverse('Pratik');
+
+// solution 2
+function reverse2(str: String): String {
+  let reversed: String = '';
+
+  for (let el of str) {
+    reversed = el + reversed;
+  }
+
+  console.log(reversed);
+
+  return reversed;
+}
+
+reverse2('pratik');
